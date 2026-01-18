83b23c04df4c5f0f4bfea7467155a9970ac58a74
Vinicius Moraes <vinicius@vsmoraes.com>
2026-01-18 17:24:08 +0100

    Fix build

--- fluxfs-spring-boot/build.gradle.kts
+++ fluxfs-spring-boot/build.gradle.kts
@@ -1,12 +1,19 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
-    id("org.springframework.boot")
     id("io.spring.dependency-management")
+    id("org.springframework.boot")
 }
 
-repositories {
-    mavenCentral()
+afterEvaluate {
+    tasks
+        .matching {
+            it.name.contains("ktlint", ignoreCase = true)
+        }.configureEach {
+            enabled = false
+        }
 }
 
 dependencies {
@@ -15,10 +22,18 @@ dependencies {
     compileOnly(project(":fluxfs-local"))
 
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-autoconfigure")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     compileOnly("aws.sdk.kotlin:s3:1.5.122")
+}
+
+tasks.named<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
 }
 
 tasks.withType<Test> {
